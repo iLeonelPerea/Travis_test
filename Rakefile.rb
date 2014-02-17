47cf4d78c6576328a59e8da859e30f0ad89faa5f
Leonel Perea <iticleonel.len@gmail.com>
2014-02-17 11:13:24 -0600

    try to..

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,7 +1,7 @@
-require "./hamming_test.rb"
+require_relative "hamming_test.rb"
 
 task :default => [:test]
 
 task :test do
-	test_complete_hamming_distance_of_for_single_nucleotide_strand
+	test_small_hamming_distance
 end
