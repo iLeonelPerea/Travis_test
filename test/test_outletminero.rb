6175cc569d4d7fb8e6189d0ded22912bf903e53c
Leonel Perea <iticleonel.len@gmail.com>
2014-02-17 23:37:42 -0600

    add phantomjs

--- test/test_outletminero.rb
+++ test/test_outletminero.rb
@@ -6,7 +6,7 @@ require "test/unit"
 class TestOutletminero < Test::Unit::TestCase
 
   def setup
-    @driver = Selenium::WebDriver.for :firefox
+    @driver = Selenium::WebDriver.for :phantomjs
     @base_url = "http://outletminero.org/"
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
