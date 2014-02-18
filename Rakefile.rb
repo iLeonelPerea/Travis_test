75558f995a25702e5baf77ae2ad16d10a17df2f6
Leonel Perea <iticleonel.len@gmail.com>
2014-02-17 23:19:29 -0600

    selenium test

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,11 @@
+require 'rake/testtask'
+
 task :default => [:test]
-task :test do
-	ruby "hamming_test.rb"
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'test'
+
+  # ensure the sample test file is included here
+  test.test_files = FileList['test/test_*.rb']
+
+  test.verbose = true
 end
